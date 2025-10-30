26e29c74ddb23228c793e42a627aef0dcc7b1657
leo <leo@funble.kr>
2025-10-30 17:56:54 +0900

    temp

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,26 @@
+"use client";
+
 export default function Home() {
     return (
         <div
             className="flex justify-center"
             style={{ backgroundColor: "#333", minHeight: "100vh" }}
         >
+            <img src="/img/join.png" alt="logo" style={{ width: "100%" }} />
             <button
                 className="submit-button"
-                style={{ width: 252, height: 54, backgroundColor: "#EFA64C" }}
+                style={{
+                    width: 252,
+                    height: 54,
+                    bottom: "50px",
+                    backgroundColor: "#EFA64C",
+                    position: "fixed",
+                }}
+                onClick={() => {
+                    alert("지금 서비스가 준비중이에요!");
+                }}
             >
-                지금 참여하기
+                지금 시작하기
             </button>
         </div>
     );
