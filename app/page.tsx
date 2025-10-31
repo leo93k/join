0e2c67fea434fe140adc875b41d676aa42dbeda9
leo <leo@funble.kr>
2025-10-31 09:20:30 +0900

    temp

--- app/page.tsx
+++ app/page.tsx
@@ -7,21 +7,39 @@ export default function Home() {
             style={{ backgroundColor: "#333", minHeight: "100vh" }}
         >
             <img src="/img/join.png" alt="logo" style={{ width: "100%" }} />
-            <button
-                className="submit-button"
+
+            <div
                 style={{
-                    width: 252,
-                    height: 54,
-                    bottom: "50px",
-                    backgroundColor: "#EFA64C",
                     position: "fixed",
-                }}
-                onClick={() => {
-                    alert("지금 서비스가 준비중이에요!");
+                    bottom: "0px",
+                    width: "100%",
+                    display: "flex",
+                    height: "90px",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    background:
+                        "linear-gradient(rgba(0,0,0,0.0), rgba(0,0,0,0.9), rgba(0,0,0,1)) ",
+                    padding: "0 20px",
                 }}
             >
-                지금 시작하기
-            </button>
+                <button
+                    className="submit-button"
+                    style={{
+                        width: "100%",
+                        height: 54,
+                        background: "#4CEF82",
+                        borderRadius: "300px",
+                        fontWeight: "bold",
+                        color: "#000",
+                        fontSize: "16px",
+                    }}
+                    onClick={() => {
+                        alert("지금 서비스가 준비중이에요!");
+                    }}
+                >
+                    지금 시작하기
+                </button>
+            </div>
         </div>
     );
 }
